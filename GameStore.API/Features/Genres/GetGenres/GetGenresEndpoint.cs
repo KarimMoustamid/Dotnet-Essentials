b75c4868c9214069468475718910ed94ff654ed6
KarimMoustamid <moustamid.teck@gmail.com>
2025-12-19 15:52:44 -0600

    Optimize GetGenres endpoint by using AsNoTracking for improved performance

--- GameStore.API/Features/Genres/GetGenres/GetGenresEndpoint.cs
+++ GameStore.API/Features/Genres/GetGenres/GetGenresEndpoint.cs
@@ -3,13 +3,14 @@ namespace GameStore.API.Features.Genres.GetGenres
     using Data;
     using Dtos;
     using System.Linq;
+    using Microsoft.EntityFrameworkCore;
 
     public static class GetGenresEndpoint
     {
         public static void MapGetGenres(this IEndpointRouteBuilder app)
         {
             app.MapGet("/", (GameStoreContext dbContext) =>
-                dbContext.Genres.Select(g => new GenreDto(g.Id, g.Name)));
+                dbContext.Genres.Select(g => new GenreDto(g.Id, g.Name)).AsNoTracking());
         }
     }
 }
