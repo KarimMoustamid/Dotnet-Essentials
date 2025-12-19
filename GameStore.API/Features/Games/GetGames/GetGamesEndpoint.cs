8355787c66379b4b412b92936ab219a5ea0a05ac
KarimMoustamid <moustamid.teck@gmail.com>
2025-12-19 15:46:43 -0600

    Refactor GetGames endpoint to use GameStoreContext and include genre information

--- GameStore.API/Features/Games/GetGames/GetGamesEndpoint.cs
+++ GameStore.API/Features/Games/GetGames/GetGamesEndpoint.cs
@@ -2,19 +2,20 @@ namespace GameStore.API.Features.Games.GetGames
 {
     using Data;
     using Dtos;
+    using Microsoft.EntityFrameworkCore;
 
     public static class GetGamesEndpoint
     {
         public static void MapGetGames(this IEndpointRouteBuilder app)
         {
-            app.MapGet("/", (GameStoreData store) => store.GetAllGames()
+            app.MapGet("/", (GameStoreContext dbContext) => dbContext.Games.Include(game => game.Genre)
                 .Select(game => new GetGamesDto(
                     game.Id,
                     game.Name,
-                    game.Genre?.Name ?? string.Empty,
+                    game.Genre!.Name,
                     game.Price,
                     game.ReleaseDate
-                )));
+                )).AsNoTracking());
         }
     }
 }
